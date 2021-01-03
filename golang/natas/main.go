66f5a3368c23ad103bbf2e394e8bf5e78532cadd
bogey3 <sam@sbogart.com>
2021-01-03 17:25:59 -0500

    Updated main.go with timing and clearer heading for password

--- golang/natas/main.go
+++ golang/natas/main.go
@@ -1,22 +1,21 @@
-package natas
-
+package main
 
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
-
-	fmt.Printf("%-8s | %-10s | %-8s | %-0s\n", "Puzzle", "Solved", "Username", "Password")
-	fmt.Println("===================================================================")
+	fmt.Printf("%-8s | %-10s | %-13s | %-8s | %-0s\n", "Puzzle", "Solved", "Duration", "Username", "Next Password")
+	fmt.Println("==================================================================================")
 	passwords := []string{"natas0"}
 	Init()
 	for i, solution := range(Solutions){
-		fmt.Printf("%-8s | %-10s | %-8s | %-0s", "Natas " + strconv.Itoa(i), "Not Solved", "natas" + strconv.Itoa(i), "")
+		start := time.Now()
+		fmt.Printf("%-8s | %-10s | %-13s | %-8s | %-0s", "Natas " + strconv.Itoa(i), "Not Solved", "", "natas" + strconv.Itoa(i), "")
 		newPass := solution("natas" + strconv.Itoa(i), passwords[i])
 		passwords = append(passwords, newPass)
-		fmt.Printf("\r%-8s | %-10s | %-8s | %-0s\n", "Natas " + strconv.Itoa(i), "Solved", "natas" + strconv.Itoa(i), newPass)
+		fmt.Printf("\r%-8s | %-10s | %-13s | %-8s | %-0s\n", "Natas " + strconv.Itoa(i), "Solved", time.Since(start), "natas" + strconv.Itoa(i), newPass)
 	}
-
 }
